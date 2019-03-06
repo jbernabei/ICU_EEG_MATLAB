dd4612cfb04919a48f17eb86cf0ee64e889c2281
John Bernabei <jbernabei@huntsman-ve505-0790.apn.wlan.upenn.edu>
2019-03-05 20:33:28 -0500

    fixed merge conflicts

--- ICU_EEG_ML.m
+++ ICU_EEG_ML.m
@@ -38,7 +38,6 @@ end
 for i = 1:num_patients
     session = IEEGSession(all_annots(i).patient, iEEGid, iEEGpw); % Initiate IEEG session
     sampleRate = session.data.sampleRate; % Sampling rate
-    data_clip(i).data = session.data.getvalues(1:(15*60*sampleRate),channels)';
     %data_clip(i).data = session.data.getvalues(1:(15*60*sampleRate),channels);
     data_with_NaN(i).data = session.data.getvalues(1:(80*60*sampleRate),channels);
 end
